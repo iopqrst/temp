98015f4401bb01e36e381ada119e1558ff4ce373
iopqrst <opqrst@163.com>
2014-12-08 17:09:09 +0800

    代码封装01

---
+++ js-code01.js
@@ -0,0 +1,50 @@
+var d = document, w = window;
+
+/**
+ * Get element by id
+ */	
+function get(element){
+	if (typeof element == "string")
+		element = d.getElementById(element);
+	return element;
+}
+
+/**
+ * Attaches event to a dom element
+ */
+function addEvent(el, type, fn){
+	if (w.addEventListener){
+		el.addEventListener(type, fn, false);
+	} else if (w.attachEvent){
+		var f = function(){
+		  fn.call(el, w.event);
+		};			
+		el.attachEvent('on' + type, f)
+	}
+}
+
+/**
+ * Creates and returns element from html chunk
+ */
+var toElement = function(){
+	var div = d.createElement('div');
+	return function(html){
+		div.innerHTML = html;
+		var el = div.childNodes[0];
+		div.removeChild(el);
+		return el;
+	}
+}();
+
+function hasClass(ele,cls){
+	return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+}
+
+function addClass(ele,cls) {
+	if (!hasClass(ele,cls)) ele.className += " "+cls;
+}
+
+function removeClass(ele,cls) {
+	var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+	ele.className=ele.className.replace(reg,' ');
+}
